178b239cc05293c89ec975bbade963ff2920dc1b
pasit.po <unknownladge@gmail.com>
2020-01-02 11:34:12 +0700

    [feat] add new project

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+
+	//thenew "github.com/beaw/newapi/hander" //server
+		thenew "newapi/hander" //server
+)
+
+type Article struct {
+	Id      string `json:"Id"`
+	Title   string `json:"Title"`
+	Desc    string `json:"Desc"`
+	Content string `json:"Content"`
+	Isbn    string `json:Isbn`
+}
+
+var Articles []Article
+
+func main() {
+
+	Articles = []Article{
+		Article{Id: "1", Title: "Hello 1", Desc: "Article Description", Content: "Article Content"},
+		Article{Id: "2", Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
+	}
+	thenew.ConnDB()
+	handleRequests()
+	defer thenew.CloseDB()
+}
+
+func handleRequests() {
+	// creates a new instance of a mux router
+	myRouter := mux.NewRouter().StrictSlash(true)
+	// replace http.HandleFunc with myRouter.HandleFunc
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/article", thenew.ReturnAllArticles).Methods("GET")
+	myRouter.HandleFunc("/article/{id}", thenew.ReturnSingleArticle).Methods("GET")
+	myRouter.HandleFunc("/article", thenew.CreateNewArticle).Methods("POST")
+	myRouter.HandleFunc("/article/{id}", thenew.DeleteArticle).Methods("DELETE")
+	myRouter.HandleFunc("/article/{id}", thenew.UpdateArticle).Methods("PUT")
+	// finally, instead of passing in nil, we want
+	// to pass in our newly created router as the second
+	// argument		
+	log.Fatal(http.ListenAndServe(":10000", myRouter))
+}
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	//fmt.Println("Endpoint Hit: homePage")
+}
