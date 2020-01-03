cc61bcb5c10b5941db13ce45a795fcbff154099a
pasit.po <unknownladge@gmail.com>
2020-01-03 10:54:37 +0700

    [test] add dummy data

--- hander/database.go
+++ hander/database.go
@@ -1,164 +1,41 @@
 package hander
 
-// var datasetarticle Article [{
-//         "Id": "0",
-//         "Title": "zero",
-//         "Desc": "test",
-//         "Content": "Content",
-//         "ISBN": "000-0-00-00000-0",
-//         "Time": "2019-12-23T05:29:16Z",
-//         "Recentupdate": "2019-12-23T05:27:59Z"
-//     },
-//     {
-//         "Id": "1",
-//         "Title": "aaaaa",
-//         "Desc": "test",
-//         "Content": "Content",
-//         "ISBN": "011-1-12-12345-1",
-//         "Time": "2019-12-24T09:12:35Z",
-//         "Recentupdate": "2019-12-24T09:12:35Z"
-//     },
-//     {
-//         "Id": "11",
-//         "Title": "eleven",
-//         "Desc": "test",
-//         "Content": "Content",
-//         "ISBN": "000-0-00-00667-6",
-//         "Time": "2019-12-23T08:29:03Z",
-//         "Recentupdate": "2019-12-23T08:29:03Z"
-//     },
-//     {
-//         "Id": "12",
-//         "Title": "twelve",
-//         "Desc": "test",
-//         "Content": "Content",
-//         "ISBN": "000-0-00-00667-6",
-//         "Time": "2019-12-23T08:30:24Z",
-//         "Recentupdate": "2019-12-23T08:30:24Z"
-//     },
-//     {
-//         "Id": "13",
-//         "Title": "one",
-//         "Desc": "33",
-//         "Content": "Content",
-//         "ISBN": "112-3-23-22312-3",
-//         "Time": "2019-12-24T07:47:31Z",
-//         "Recentupdate": "2019-12-24T07:47:31Z"
-//     },
-//     {
-//         "Id": "14",
-//         "Title": "one",
-//         "Desc": "33",
-//         "Content": "Content",
-//         "ISBN": "000-0-00-00022-1",
-//         "Time": "2019-12-24T07:25:59Z",
-//         "Recentupdate": "2019-12-24T07:25:59Z"
-//     },
-//     {
-//         "Id": "16",
-//         "Title": "aaaaa",
-//         "Desc": "test",
-//         "Content": "c",
-//         "ISBN": "011-1-12-12345-1",
-//         "Time": "2019-12-24T09:19:06Z",
-//         "Recentupdate": "2019-12-24T09:19:06Z"
-//     },
-//     {
-//         "Id": "17",
-//         "Title": "five",
-//         "Desc": "15",
-//         "Content": "Content",
-//         "ISBN": "123-3-21-23211-0",
-//         "Time": "2019-12-26T04:41:21Z",
-//         "Recentupdate": "2019-12-26T04:41:21Z"
-//     },
-//     {
-//         "Id": "18",
-//         "Title": "eighteen",
-//         "Desc": "15",
-//         "Content": "Content",
-//         "ISBN": "123-3-21-23211-0",
-//         "Time": "2019-12-26T04:41:30Z",
-//         "Recentupdate": "2019-12-26T04:41:43Z"
-//     },
-//     {
-//         "Id": "2",
-//         "Title": "two",
-//         "Desc": "test",
-//         "Content": "Content",
-//         "ISBN": "000-0-00-00667-6",
-//         "Time": "2019-12-23T08:26:29Z",
-//         "Recentupdate": "2019-12-23T08:26:29Z"
-//     },
-//     {
-//         "Id": "20",
-//         "Title": "TWENTYyyy",
-//         "Desc": "55555",
-//         "Content": "c21",
-//         "ISBN": "123-2-31-34564-1",
-//         "Time": "2019-12-26T07:28:50Z",
-//         "Recentupdate": "2019-12-26T07:29:29Z"
-//     },
-//     {
-//         "Id": "3",
-//         "Title": "three",
-//         "Desc": "33",
-//         "Content": "Content",
-//         "ISBN": "033-3-33-33333-3",
-//         "Time": "2019-12-23T07:34:27Z",
-//         "Recentupdate": "2019-12-24T08:08:49Z"
-//     },
-//     {
-//         "Id": "4",
-//         "Title": "four",
-//         "Desc": "test",
-//         "Content": "Content",
-//         "ISBN": "077-7-77-77777-7",
-//         "Time": "2019-12-23T07:38:03Z",
-//         "Recentupdate": "2019-12-23T07:38:03Z"
-//     },
-//     {
-//         "Id": "5",
-//         "Title": "five",
-//         "Desc": "test",
-//         "Content": "Content",
-//         "ISBN": "000-0-00-00667-6",
-//         "Time": "2019-12-23T08:02:28Z",
-//         "Recentupdate": "2019-12-23T08:02:28Z"
-//     },
-//     {
-//         "Id": "6",
-//         "Title": "six",
-//         "Desc": "test",
-//         "Content": "Content",
-//         "ISBN": "000-0-00-00066-6",
-//         "Time": "2019-12-23T07:54:46Z",
-//         "Recentupdate": "2019-12-23T07:54:46Z"
-//     },
-//     {
-//         "Id": "7",
-//         "Title": "seven",
-//         "Desc": "test",
-//         "Content": "Content",
-//         "ISBN": "000-0-00-00667-6",
-//         "Time": "2019-12-23T08:25:26Z",
-//         "Recentupdate": "2019-12-23T08:25:26Z"
-//     },
-//     {
-//         "Id": "8",
-//         "Title": "eight",
-//         "Desc": "test",
-//         "Content": "Content",
-//         "ISBN": "000-0-00-00667-6",
-//         "Time": "2019-12-23T08:25:55Z",
-//         "Recentupdate": "2019-12-23T08:25:55Z"
-//     },
-//     {
-//         "Id": "9",
-//         "Title": "nine",
-//         "Desc": "tree",
-//         "Content": "s",
-//         "ISBN": "333-4-44-77684-5",
-//         "Time": "2019-12-23T05:24:38Z",
-// 		"Recentupdate": "2019-12-23T05:}
-// 	]
+const (
+	DB0 = `{
+    "Id": "0",
+    "Title": "zero",
+    "Desc": "test",
+    "Content": "Content",
+    "ISBN": "000-0-00-00000-0",
+    "Time": "2019-12-23T05:29:16Z",
+    "Recentupdate": "2019-12-23T05:27:59Z"
+}`
+	DB1 = ` {
+    "Id": "1",
+    "Title": "one",
+    "Desc": "test",
+    "Content": "Content",
+    "ISBN": "011-1-12-12345-1",
+    "Time": "2019-12-24T09:12:35Z",
+    "Recentupdate": "2019-12-24T09:12:35Z"
+}`
+	DB11 = `    {
+    "Id": "11",
+    "Title": "eleven",
+    "Desc": "test",
+    "Content": "Content",
+    "ISBN": "000-0-00-00667-6",
+    "Time": "2019-12-23T08:29:03Z",
+    "Recentupdate": "2019-12-23T08:29:03Z"
+}`
+	DB12 = `{
+    "Id": "12",
+    "Title": "twelve",
+    "Desc": "test",
+    "Content": "Content",
+    "ISBN": "000-0-00-00667-6",
+    "Time": "2019-12-23T08:30:24Z",
+    "Recentupdate": "2019-12-23T08:30:24Z"
+}`
+	DB9 = `{"Id": "9","Title": "nine","Desc": "tree","Content": "s","ISBN": "333-4-44-77684-5","Time": "2019-12-23T05:24:38Z","Recentupdate": "2019-12-23T05:}`
+)
